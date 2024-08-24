361dd4a507722b891ded8fed64d5b2be9d77c892
NM711 <110122420+NM711@users.noreply.github.com>
2024-08-24 15:28:19 -0700

    Deleted unecessary color macros

--- generator.hpp
+++ generator.hpp
@@ -3,16 +3,6 @@
 
 #include "lib/ecautomata/ecautomata.hpp"
 
-#define RED "\033[0;31m"
-#define DARK_RED "\033[38;2;82;11;20m"
-#define DARK_GREEN "\033[38;2;35;89;18m"
-#define GREEN "\033[0;32m"
-#define BLUE  "\033[0;34m"
-#define BROWN "\033[0;33m"
-#define GRAY "\033[0;90m"
-#define LIGHT_YELLOW "\033[38;2;237;227;111m"
-#define DEFAULT "\033[0m"
-
 /*
   Rules:
 
